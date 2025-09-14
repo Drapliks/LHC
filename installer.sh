12c85e2180a25a198f50eb0516a5c803750ee604
Drapliks <kinstevg@gmail.com>
2025-09-14 18:44:15 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S mako slurp fastfetch pavucontrol fish waybar mousepad cava udiskie ttf-font-awesome otf-font-awesome adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts
+sudo pacman -S mako slurp fastfetch pavucontrol fish waybar mousepad cava wl-clipboard wl-clip-persist cliphist udiskie ttf-font-awesome otf-font-awesome adobe-source-han-sans-jp-fonts adobe-source-han-sans-cn-fonts
 cd
 git clone https://aur.archlinux.org/yay.git
 cd yay
